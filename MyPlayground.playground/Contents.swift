7734902a2fd41f51e5228ecf9d313ce8b21abe2c
MTA-MAC <mta-mac@MTA-MACs-iMac-4.local>
2020-03-12 12:59:42 +0700

    การบ้านข้อ 1

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,15 @@
 import UIKit
 
 var str = "Hello, playground"
+
+var InputNumber1 : Double = 5
+var InputNumber2 : Double = 4
+var Summary = (InputNumber1+InputNumber2)
+var Different = (InputNumber1-InputNumber2)
+var Multiply = (InputNumber1*InputNumber2)
+var Divide = (InputNumber1/InputNumber2)
+
+print("Summary =",Summary)
+print("Different =",Different)
+print("Multiply =",Multiply)
+print("Divide =",Divide)
